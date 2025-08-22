50b484d6031aa6d5af63f268104b86f492b10ba1
danielffnando <daniel.f.m.soares@gmail.com>
2025-08-21 22:53:48 -0300

    Add files via upload
    - Trying to make it more compatible of OOP standards
    - Adding todo's for future changes

--- tasks.py
+++ tasks.py
@@ -1,164 +1,202 @@
 import numpy
 import sys
+import configparser
 
-tasknum=1
-taskarray = numpy.array(['n','Task','Status'])
+task_array = numpy.array(['n','Task','Status'])
+task_config = configparser.ConfigParser()
+loop = True
+defaut_config = ['status_scope=simple',
+                 'table_align=left',
+                 'subtasks=false',
+                 'autosave=true',
+                 'show=all',
+                 'done_tasks=keep',
+                 'run_withou_args=open',
+                 'screen_clean=true']
+# TODO Options:
+# Status: To Do, In Progress, Done// To Do, Done
+# Table: Left align, centered, right align
+# Sub tasks (1.1, 1.1.1, etc)
+# Autosave=(true,false)
+# show=all, todo/in prongress
+# done tasks = keep, delete, archive (archive will give special index numeber, maybe a.1 or a1)
+# run_withou_args = open, open with new, list
+# screen_clean = true, false
 
-def TasksFileLoad():
-    global taskarray
-    try:
-        taskarray = numpy.loadtxt('tasks.txt', dtype='str', delimiter=',')
-    except:
-        print('Tasks file not found, creating new tasks file...')
+
+class TaskFiles:
+    def TaskFileLoad(self, array): # TODO 'self'
+        global task_array
         try:
-            TasksFileSave()
-        except:
-            print('Tasks file creation failed')
-            sys.exit()
-        else:
-            print('New tasks file created')
-            
-def TasksFileSave():
-    try:
-        numpy.savetxt('tasks.txt', taskarray, delimiter=',', fmt='%s')
-    except:
-        print('Failed to save')
-
-def TaskList():
-    #print(taskarray,"\n")
-    TableDraw()
-
-def TaskNew():
-    global taskarray
-    global tasknum
-    taskname = input('New task name: ')
-    taskarray = numpy.vstack((taskarray,[tasknum, taskname, 'todo']))
-    tasknum += 1
-
-def TaskEdit():
-    global taskarray
-    TaskList()
-    try:
-        to_edit = int(input('Task to edit ("0" to return): '))
-    except:
-        print('Please type the task index number')
-        TaskEdit()
-    else:
-        if to_edit == 0:
-            return
-        elif to_edit > numpy.shape(taskarray)[0]-1:
-            print('Please use an index number corrently in use\n')
-            TaskEdit()
-        else:
-            to_change = input('Change task name or status? "back" to return): ')
-            if to_change == 'name':
-                new_name = input('New task name: ')
-                taskarray[to_edit][1] = new_name
-            elif to_change == 'status':
-                new_status = input('New task status (done, todo): ')
-                taskarray[to_edit][2] = new_status
-            elif to_change == 'back':
-                print('potato')
-                TaskEdit()
+            task_array = numpy.loadtxt('tasks.txt', dtype='str', delimiter=',')
+        except FileNotFoundError:
+            print('Tasks file not found, creating new tasks file...')
+            try:
+                TaskFiles.TaskFileSave()
+            except:
+                print('Tasks file creation failed')
+                sys.exit()
             else:
-                print('Please choose a valid option\n')
-                TaskEdit()
+                print('New tasks file created')
                 
-def TaskDelete():
-    global taskarray
-    TaskList()
-    try:
-        to_delete = int(input('Task to delete ("0" to return): '))
-    except:
-        print('Please type the task index number\n')
-        TaskDelete()
-    else:
-        if to_delete == 0:
+    def TaskFileSave():
+        try:
+            numpy.savetxt('tasks.txt', task_array, delimiter=',', fmt='%s')
+        except:
+            print('Failed to save')
+
+class TaskCommands:
+    def TaskList():
+        TaskTableDraw.TableDraw()
+
+    def TaskNew(): # TODO 'self'
+        global task_array
+        taskname = input('New task name: ')
+        if taskname == '':
             return
-        elif to_delete > numpy.shape(taskarray)[0]-1:
-            print('Please use an index number corrently in use\n')
+        task_array = numpy.vstack((task_array,['1970', taskname, 'To Do']))
+
+    def TaskEdit(): # TODO 'self'
+        global task_array
+        TaskCommands.TaskList()
+        try:
+            to_edit = int(input('Task to edit (leave empty to return): '))
+        except:
+            print('Please type the task index number')
+            TaskCommands.TaskEdit()
+        else: # TODO match case
+            if to_edit == '':
+                return
+            elif to_edit == 0:
+                return
+            elif to_edit > numpy.shape(task_array)[0]-1:
+                print('Please use an index number corrently in use\n')
+                TaskCommands.TaskEdit()
+            else: # TODO match case
+                to_change = input('Change task name or status? (Leave empty to return): ')
+                if to_change == 'name':
+                    new_name = input('New task name: ')
+                    task_array[to_edit][1] = new_name
+                elif to_change == 'status':
+                    new_status = input('New task status (done, todo): ')
+                    task_array[to_edit][2] = new_status
+                elif to_change == '':
+                    TaskCommands.TaskEdit()
+                else:
+                    print('Please choose a valid option\n')
+                    TaskCommands.TaskEdit()
+                    
+    def TaskDelete(): # TODO 'self'
+        global task_array
+        TaskCommands.TaskList()
+        try:
+            to_delete = int(input('Task to delete (Leave empty to return): '))
+        except:
+            print('Please type the task index number\n')
             TaskDelete()
         else:
-            taskarray = numpy.delete(taskarray,to_delete,axis=0)
+            if to_delete == '':
+                return
+            elif to_delete == 0:
+                return
+            elif to_delete > numpy.shape(task_array)[0]-1:
+                print('Please use an index number corrently in use\n')
+                TaskDelete()
+            else:
+                task_array = numpy.delete(task_array,to_delete,axis=0)
 
-def Commands(comm):
+class TaskUtils:
+    def UpdateTasksIndexNumber(): # TODO 'self'
+        global task_array
+        for i in range(numpy.shape(task_array)[0]):
+            try:
+                if task_array[i][0] != 'n' and task_array[i][0] != i:
+                    task_array[i][0] = i
+            except:
+                break
+        
+class TaskTableDraw:
+    def ArrayCharWidth(array): # TODO 'self'
+        table_line_size = 0
+        longer_line = []
+        try:
+            for col in range(numpy.shape(array)[1]):
+                longer_line.append(max(array[:, col], key=len))
+        except IndexError:
+            longer_line = array
+        table_line_size = 0
+        for i in range(len(longer_line)):
+            table_line_size = table_line_size + len(longer_line[i])
+        return table_line_size, longer_line
+
+    def DrawDividingLine(size): # TODO 'self'
+        to_print = '-' * size
+        return to_print
+        
+    def PrintLineWithVerticalSpacer(array, longer_line): # TODO 'self'
+        to_print = '|'
+        for cell in range(numpy.shape(array)[0]):
+            space_buffer_size = len(longer_line[cell]) - len(array[cell])
+            space_buffer = ' ' * space_buffer_size
+            to_print = to_print + array[cell] + space_buffer + '|'
+        return to_print
+
+    def TableDraw(): # TODO 'self'
+        try:
+            number_table_row_column_cross = numpy.shape(task_array)[1] + 1
+        except:
+            number_table_row_column_cross = 4
+        
+        array_char_width = TaskTableDraw.ArrayCharWidth(task_array)[0]
+        array_longer_line = TaskTableDraw.ArrayCharWidth(task_array)[1]
+        table_row_num_char_with_spacer = array_char_width + number_table_row_column_cross
+        table_column_num_char = numpy.shape(task_array)[0]
+        
+        print(TaskTableDraw.DrawDividingLine(table_row_num_char_with_spacer))
+        try:
+            for line in range(0, table_column_num_char):
+                print(TaskTableDraw.PrintLineWithVerticalSpacer(task_array[line], array_longer_line))
+        except IndexError:
+            table_column_num_char = numpy.shape(task_array)[0]
+            for line in range(0, table_column_num_char-2):
+                print(TaskTableDraw.PrintLineWithVerticalSpacer(task_array, array_longer_line))
+        print(TaskTableDraw.DrawDividingLine(table_row_num_char_with_spacer))
+
+
+def Commands(comm): # TODO 'self'
+    global loop
     match comm:
         case 'List' | 'list' | 'l':
-            TaskList()
+            TaskCommands.TaskList()
         case 'New' | 'new' | 'n':
-            TaskNew()
-            UpdateTasksIndexNumber()
-            TaskList()
+            TaskCommands.TaskNew()
+            TaskUtils.UpdateTasksIndexNumber()
+            TaskCommands.TaskList()
         case 'Edit' | 'edit' | 'e':
-            TaskEdit()
-            UpdateTasksIndexNumber()
-            TaskList()
+            TaskCommands.TaskEdit()
+            TaskUtils.UpdateTasksIndexNumber()
+            TaskCommands.TaskList()
         case 'Delete' | 'delete' | 'd':
-            TaskDelete()
-            UpdateTasksIndexNumber()
-            TaskList()
+            TaskCommands.TaskDelete()
+            TaskUtils.UpdateTasksIndexNumber()
+            TaskCommands.TaskList()
+        # TODO Undo: undo last edit
+        # TODO Config: edit program options
+        # TODO Save: save current tasklist
+        # TODO Save and exit: save current tasklist and exit
+        # TODO auto save when comm=ex: true, false
+        case 'Save' | 'save' | 's':
+            TaskFiles.TaskFileSave()
         case 'Exit' | 'exit' | 'ex':
-            sys.exit()
+            TaskFiles.TaskFileSave()
+            loop = False
         case _:
             return 'Please select an option'
-
-def UpdateTasksIndexNumber():
-    global taskarray
-    for i in range(numpy.shape(taskarray)[0]):
-        try:
-            if taskarray[i][0] != 'n' and taskarray[i][0] != i:
-                taskarray[i][0] = i
-        except:
-            break
         
-def ArrayCharWidth(array):
-    table_line_size = 0
-    longer_line = []
-    try:
-        for col in range(numpy.shape(array)[1]):
-            longer_line.append(max(array[:, col], key=len))
-    except IndexError:
-        longer_line = array
-    table_line_size = 0
-    for i in range(len(longer_line)):
-        table_line_size = table_line_size + len(longer_line[i])
-    return table_line_size, longer_line
+def main():
+    TaskFiles.TaskFileLoad()
+    while loop == True:
+        Commands(input('Type your option: '))
 
-def DrawDividingLine(size):
-    to_print = '-' * size
-    return to_print
-    
-def PrintLineWithVerticalSpacer(array, longer_line):
-    to_print = '|'
-    for cell in range(numpy.shape(array)[0]):
-        space_buffer_size = len(longer_line[cell]) - len(array[cell])
-        space_buffer = ' ' * space_buffer_size
-        to_print = to_print + array[cell] + space_buffer + '|'
-    return to_print
-
-def TableDraw():
-    try:
-        number_table_row_column_cross = numpy.shape(taskarray)[1] + 1
-    except:
-        number_table_row_column_cross = 4
-    
-    array_char_width = ArrayCharWidth(taskarray)[0]
-    array_longer_line = ArrayCharWidth(taskarray)[1]
-    table_row_num_char_with_spacer = array_char_width + number_table_row_column_cross
-    table_column_num_char = numpy.shape(taskarray)[0]
-    
-    print(DrawDividingLine(table_row_num_char_with_spacer))
-    try:
-        for line in range(0, table_column_num_char):
-            print(PrintLineWithVerticalSpacer(taskarray[line], array_longer_line))
-    except IndexError:
-        table_column_num_char = numpy.shape(taskarray)[0]
-        for line in range(0, table_column_num_char-2):
-            print(PrintLineWithVerticalSpacer(taskarray, array_longer_line))
-    print(DrawDividingLine(table_row_num_char_with_spacer))
-
-while 'true':
-    TasksFileLoad()
-    inp = input('Type your option: ')
-    Commands(inp)
-    TasksFileSave()
+if __name__ == '__main__':
+    main()
